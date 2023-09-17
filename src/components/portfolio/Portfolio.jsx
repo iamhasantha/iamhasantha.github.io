8f79b144728ba57caab1a7425bcae46c6cf0615e
Hasantha Kariyawasam <2020cs090@stu.ucsc.cmb.ac.lk>
2023-09-18 01:29:33 +0530

    add typewriter

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,8 @@ import {
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faComments } from "@fortawesome/free-regular-svg-icons";
+import Typewriter from "../Typewriter";
+import ParticleRing from "../Particle Ring/ParticleRing";
 
 const Portfolio = () => {
   const emailAddress = "hasanthakariyawasam@gmail.com";
@@ -20,72 +22,82 @@ const Portfolio = () => {
   };
 
   return (
-    <div className="portfolio">
-      {/* Your Image */}
-      <div className="profile-pic-container">
-        <img
-          className="profile-pic"
-          src="https://i.ibb.co/6Psd4Mb/profile.jpg"
-          alt="Hasantha Kariyawasam"
-        />
-      </div>
+    <>
+      
 
-      <div className="name-overlay">
-        <p className="name">Hasantha Kariyawasam</p>
-      </div>
+      <div className="portfolio">
+        {/* Your Image */}
+        <div className="profile-pic-container">
+          <img
+            className="profile-pic"
+            src="https://i.ibb.co/6Psd4Mb/profile.jpg"
+            alt="Hasantha Kariyawasam"
+          />
+        </div>
 
-      {/* Description */}
-      <p>A Computer Science Undergraduate @ UCSC.</p>
+        <div className="name-overlay">
+          <p className="name">Hasantha Kariyawasam</p>
+        </div>
 
-      {/* Social Media Links */}
-      <div className="social-media">
+        {/* Description */}
+        <p>
+          <Typewriter
+            text="A Computer Science Undergraduate @ UCSC. "
+            delay={100}
+          ></Typewriter>
+        </p>
+
+        {/* Social Media Links */}
+        <div className="social-media">
+          <a
+            href="https://www.linkedin.com/in/imhasantha/"
+            className="social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            href="https://www.behance.net/hasanthaka1764"
+            className="social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faBehance} />
+          </a>
+          <a
+            href="https://github.com/iamhasantha"
+            target="_blank"
+            className="social-icon"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+
+        {/* Download CV Button */}
         <a
-          href="https://www.linkedin.com/in/imhasantha/"
-          className="social-icon"
+          href="https://drive.google.com/file/d/1FiaNsWNLfTmQuD2NtmvseQzum10kPqX6/view?usp=sharing"
           target="_blank"
+          download
           rel="noreferrer"
         >
-          <FontAwesomeIcon icon={faLinkedin} />
+          <button className="download-cv-button">Download CV</button>
         </a>
-        <a
-          href="https://www.behance.net/hasanthaka1764"
-          className="social-icon"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon icon={faBehance} />
-        </a>
-        <a
-          href="https://github.com/iamhasantha"
-          target="_blank"
-          className="social-icon"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-      </div>
 
-      {/* Download CV Button */}
-      <a
-        href="https://drive.google.com/file/d/1FiaNsWNLfTmQuD2NtmvseQzum10kPqX6/view?usp=sharing"
-        target="_blank"
-        download rel="noreferrer"
-      >
-        <button className="download-cv-button">Download CV</button>
-      </a>
-
-      <div className="email-box">
-        <p>You can reach out to me at </p>
-        <span className="email-link" onClick={handleClickEmail}>
-          <FontAwesomeIcon icon={faEnvelope} className="email-icon" />
-          {emailAddress}
-        </span>
-        <span className="contact-link" onClick={handleClickPhone}>
-          <FontAwesomeIcon icon={faComments} className="contact-icon" />
-          {phoneNumber}
-        </span>
+        <div className="email-box">
+          <p>You can reach out to me at </p>
+          <span className="email-link" onClick={handleClickEmail}>
+            <FontAwesomeIcon icon={faEnvelope} className="email-icon" />
+            {emailAddress}
+          </span>
+          <span className="contact-link" onClick={handleClickPhone}>
+            <FontAwesomeIcon icon={faComments} className="contact-icon" />
+            {phoneNumber}
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
